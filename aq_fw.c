c1b3502c0265ccd52341391be595bf751c8e4524
Shea Smith <51303984+SheaSmith@users.noreply.github.com>
2024-08-04 15:58:00 +1200

    maybe?

--- aq_fw.c
+++ aq_fw.c
@@ -89,10 +89,10 @@ __FBSDID("$FreeBSD$");
 #define FEATURES_AQ2_IFACE_B0	0x40000000
 
 
-#define	__BIT(n)	(1 << (n))
+#define	__BIT(n)	(1L << (n))
 #define __BITS(hi,lo)	((~((~0)<<((hi)+1)))&((~0)<<(lo)))
 
-#define __LOWEST_SET_BIT(__mask) ((((__mask) - 1) & (__mask)) ^ (__mask))
+#define __LOWEST_SET_BIT(__mask) ((((__mask) - 1L) & (__mask)) ^ (__mask))
 #define __SHIFTOUT(__x, __mask) (((__x) & (__mask)) / __LOWEST_SET_BIT(__mask))
 #define __SHIFTIN(__x, __mask) ((__x) * __LOWEST_SET_BIT(__mask))
 
